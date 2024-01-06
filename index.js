597cbfde64ea8eb925483e62feea360ffa48f804
Erin Y Kim <erinykim04@gmail.com>
2024-01-06 13:24:33 -0800

    I added basic Home Page and created a popup functions to control amount of lives for the game and a redirection button for Start Game to take you to the game.html File

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function showOptions() {
+    let optionsPopup = document.getElementById('optionsPopup');
+    optionsPopup.style.display = 'block';
+}
+function closeOptionsPopup() {
+    let optionsPopup = document.getElementById('optionsPopup');
+    optionsPopup.style.display = 'none';
+}
 
+function redirectToGamePage() {
+    window.location.href = 'game.html'
+}
+// this function was to redirect my home page to the game page!
+
+function applyOptions() {
+    console.log('hello')
+    let totalLives = document.getElementById('totalLives').value;
+    alert('Options applied: Total Lives ' + totalLives);
+    closeOptionsPopup();
+}
