277687d78538911d3669408988373af66c008af8
Erin Y Kim <erinykim04@gmail.com>
2024-01-07 22:50:44 -0800

    I improved the title to make it have a pinpong effect and then goes into the cascade as the default

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ function applyOptions() {
     alert('Options applied: Total Lives ' + totalLives);
     closeOptionsPopup();
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+     // waits for html to load; js runs after html is parsed
+    let firstLetter = document.getElementById('firstLetter');
+    // Add an event listener for the animationend event
+    firstLetter.addEventListener('animationend', () => {
+          // Start the cascading animation for the other spans
+        document.querySelectorAll('h1 span').forEach(function(span, index){
+               // Delay the cascading animation of the first letter
+            const delay = firstLetter === span ? '1s' : `${0.1 *(index + 1)}s`;
+            span.style.animation =`cascadeBounce 2s infinite ${delay}`;
+        });
+    });
+    // You can keep your existing logic for the pingPongEffect
+    document.querySelectorAll('h1 span').forEach(function(span){
+        span.style.animation = 'pingPongEffect 2s ease-in-out 1'
+    });
+});
